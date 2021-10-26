3a87e6e7cf9dcf0b57e4338d31b5a4e1cfcb1ca6
mtabdev <dja35676@gmail.com>
2021-10-27 02:47:46 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // var index=0
+  const [c,cc]=useState(0);
+  const [work,setWork]=useState("")
+  const [liwork,setliwork] = useState([])
+
+const clear =()=>{
+
+  setliwork([])
+ cc(0);
 }
 
-export default App;
+
+const addtoarray = ()=>{
+
+  setliwork((old)=>{
+    return [...old,work]
+  })
+  cc(c+1)
+setWork(" ")
+}
+
+    const captureData = (e)=>{
+      setWork(e.target.value)
+
+    }
+
+    const deleteTodo = (id)=>{
+      
+      setliwork((old)=>{
+        return(
+          old.filter((arr,index)=>{
+            return index!==id
+          })
+        )
+      })
+      cc(c-1)
+    }
+     
+  return (
+    <>
+      
+
+<div className="wrapper">
+  <header>Todo App</header>
+  <div className="inputField">
+    <input type="text" placeholder="Add your new todo" onChange={captureData} value={work}/>
+    <button onClick={addtoarray}>{"+"}</button>
+  </div>
+  <ul className="todoList">
+   
+     
+     {
+     liwork.map((single_work,index) =>{
+                return (<>
+                  <li key={index} >{single_work} <span className="icon" ><i className="fas fa-trash" onClick={()=>deleteTodo(index)}></i></span></li>
+                  
+                  </>
+                )
+        }) 
+       }
+
+   
+  </ul>
+  <div className="footer">
+    <span>You have {c} pending tasks</span>
+    <button onClick={clear} type="button">Clear All</button>
+  </div>
+</div>
+
+
+
+<footer>- By Mohammad Tabish</footer>
+
+
+
+  
+    </>
+  )
+}
+
+export default App
